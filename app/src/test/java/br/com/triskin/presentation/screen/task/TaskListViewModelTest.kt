721718cae99375864d79f87d26ed5bf30f7e8ab4
Daniel Santos <daniel_ap21@yahoo.com.br>
2026-05-25 01:30:46 -0300

    Fix missing expectedDate parameter in TaskListViewModelTest and add Gradle wrapper
    The SubmitForm calls were missing the expectedDate parameter added to
    TaskListIntent, causing compilation failure. Also generates the Gradle
    wrapper files that were missing from the repository.

--- app/src/test/java/br/com/triskin/presentation/screen/task/TaskListViewModelTest.kt
+++ app/src/test/java/br/com/triskin/presentation/screen/task/TaskListViewModelTest.kt
@@ -86,6 +86,7 @@ class TaskListViewModelTest {
                 talhao = "T-12",
                 description = "",
                 priority = TaskPriority.MEDIUM,
+                expectedDate = LocalDate.of(2025, 5, 25),
                 expectedTime = LocalTime.of(8, 0),
             ),
         )
@@ -111,6 +112,7 @@ class TaskListViewModelTest {
                 talhao = "T-13",
                 description = "atualizado",
                 priority = TaskPriority.HIGH,
+                expectedDate = LocalDate.of(2025, 5, 25),
                 expectedTime = LocalTime.of(9, 0),
             ),
         )
